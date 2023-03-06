9133a1203ed92efad020d9ef9ed229c524d7c46a
jonathanrichardss <jonathanrichard45@hotmail.com>
2023-03-06 14:50:48 -0300

    Valida usuário, ajustes.

--- src/Pages/SignIn/SignIn.tsx
+++ src/Pages/SignIn/SignIn.tsx
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SignInStyle } from "./styles";
 import { Link } from "react-router-dom";
+import { User } from "../../components/User/User";
 
 export function SignIn() {
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [autentication, setAutentication] = useState({});
 
-  function getUserName() {
-    return setUserName(username);
+  function getAutentication(username: String, password: String) {
+    fetch("http://localhost:8080/users").then((res) => res.json());
   }
+
+  useEffect(() => {
+    async function getAutentication(username: String, password: String) {
+      const req = await fetch("http://localhost:8080/users/users/list-all");
+
+      const responseJSON = await req.json();
+
+      setAutentication(responseJSON);
+    }
+
+    if (autentication.username) {
+      return;
+    }
+    getAutentication(username, password);
+    console.log("Roda a cada renderização.");
+    console.log(getAutentication);
+  }, [autentication]);
 
   return (
     <SignInStyle>
@@ -21,8 +40,18 @@ export function SignIn() {
           onChange={(e) => setUserName(e.target.value)}
         />
         <label htmlFor="password"></label>
-        <input type="text" id="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <input
+          type="text"
+          id="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button
+          type="submit"
+          onSubmit={() => getAutentication(username, password)}
+        >
+          Entrar
+        </button>
         <Link to="/signup">
           <p>Novo(a) aqui? Cadastre-se!</p>
         </Link>
